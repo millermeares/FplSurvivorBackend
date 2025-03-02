e3a3db2a78c28ee9e3a34309f5e1c5569a734ecc
Miller Meares <benmillermeares3@gmail.com>
2025-03-02 15:48:46 -0500

    Working /castaways link

--- lambda_functions/myFunction/index.mjs
+++ lambda_functions/myFunction/index.mjs
@@ -97,28 +97,29 @@ async function dsql_sample(token, endpoint) {
 
 
 async function getCastaways(client, args) {
-  const res = await client.query(`SELECT id, name, season, iamge_url, _fk_week_eliminated FROM survivor.castaway;`)
+  const res = await client.query(`SELECT id, name, season, image_url, _fk_week_eliminated FROM survivor.castaway;`)
   return {
     statusCode: 200,
-    body: res.rows
+    body: JSON.stringify(res.rows)
   }
 }
 
 const methodBank = {
-  'castaways': getCastaways
+  '/castaways': getCastaways
 }
 
 async function execute(event, client) {
   if (!methodBank[event['path']]) {
+    console.log(`Could not find registered method for ${event['path']}`)
     return {
       statusCode: 404,
       body: "Path not found"
     }
   }
-  method = methodBank[event['path']]
+  const method = methodBank[event['path']]
   try {
     await client.connect()
-    return method(client, event['body'])
+    return await method(client, event['body'])
   } catch (err) {
     console.log("Error when executing path")
     console.error(err)
